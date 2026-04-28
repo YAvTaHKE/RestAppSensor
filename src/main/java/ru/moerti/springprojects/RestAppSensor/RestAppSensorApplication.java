25414bbc44cb80461ce7f4d6c3c3b575c403f69e
Moerti <Moertiac@gmail.com>
2026-04-28 12:07:50 +0300

    First commit

---
+++ src/main/java/ru/moerti/springprojects/RestAppSensor/RestAppSensorApplication.java
@@ -0,0 +1,13 @@
+package ru.moerti.springprojects.RestAppSensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestAppSensorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestAppSensorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/moerti/springprojects/RestAppSensor/RestAppSensorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.moerti.springprojects.RestAppSensor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestAppSensorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
