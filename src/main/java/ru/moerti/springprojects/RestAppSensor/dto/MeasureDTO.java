8a65e5b5223d64f3d51e49c51757ebcd566858b1
Moerti <Moertiac@gmail.com>
2026-04-29 17:19:40 +0300

    DTO finish

--- src/main/java/ru/moerti/springprojects/RestAppSensor/dto/MeasureDTO.java
+++ src/main/java/ru/moerti/springprojects/RestAppSensor/dto/MeasureDTO.java
@@ -1,4 +1,63 @@
 package ru.moerti.springprojects.RestAppSensor.dto;
 
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.*;
+
+import java.math.BigDecimal;
 public class MeasureDTO {
+
+    @NotNull(message = "Value cannot be null")
+    @Min(value = -100, message = "Value should be greater than or equal -100")
+    @Max(value = 100, message = "Value should be less than or equal 100")
+    private BigDecimal value;
+
+    @NotNull(message = "Raining cannot be null")
+    private Boolean raining;
+
+    @Column(name = "name", nullable = false, unique = true, length = 30)
+    @NotBlank(message = "Name not should be empty")
+    @Size(min = 3, max = 30, message = "Name should between 3 and 30 characters")
+    private String  sensorName;
+
+    public MeasureDTO() {
+    }
+
+    public MeasureDTO(BigDecimal value, Boolean raining, String sensorName) {
+        this.value = value;
+        this.raining = raining;
+        this.sensorName = sensorName;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+    public Boolean getRaining() {
+        return raining;
+    }
+
+    public void setRaining(Boolean raining) {
+        this.raining = raining;
+    }
+
+    public String getSensorName() {
+        return sensorName;
+    }
+
+    public void setSensorName(String sensorName) {
+        this.sensorName = sensorName;
+    }
+
+    @Override
+    public String toString() {
+        return "MeasureDTO{" +
+                "value=" + value +
+                ", raining=" + raining +
+                ", sensorId=" + sensorName +
+                '}';
+    }
 }
--- src/main/java/ru/moerti/springprojects/RestAppSensor/dto/SensorDTO.java
+++ src/main/java/ru/moerti/springprojects/RestAppSensor/dto/SensorDTO.java
@@ -1,4 +1,36 @@
 package ru.moerti.springprojects.RestAppSensor.dto;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class SensorDTO {
+
+    @Column(name = "name", nullable = false, unique = true, length = 30)
+    @NotBlank(message = "Name not should be empty")
+    @Size(min = 3, max = 30, message = "Name should between 3 and 30 characters")
+    private String name;
+
+    public SensorDTO(){
+
+    }
+
+    public SensorDTO(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "SensorDTO{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
